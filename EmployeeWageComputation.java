68faa0873a120a2f576b224499316b2b23e54e37
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-25 16:58:12 +0530

    Add:Welcome message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+public class EmployeeWageComputation						//Main class
+{
+	public static void main(String[] args)					//Main method
+	{
+		System.out.println("Welcome to employee wage computation");
+	}
+}
