fee8f521c98b59d556917a44b537db7a8dba7d3f
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-25 17:49:20 +0530

    Add:Employee wages for multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,48 +1,47 @@
-public class EmployeeWageComputation						//Main class	
+public class EmployeeWageComputation                               //Main class
 {
 	//Constants
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
-	public static final int EMP_RATE_PER_HR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_WORKING_HRS=100;
 
-	public static int computeEmpwage()					//Method to calulate Total Employee Wage
+	public static void computeEmpWage(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)    //Method 
 	{
-		System.out.println("Welcome to employee wage computation");
-		//Variables		
+		//Variables
 		int empHrs=0;
-		int totalEmpWage=0;
-		int empWage=0;
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;	
-		//while loop for less than 20 working days and less than 100 working hours
-		while(totalEmpHrs<=MAX_WORKING_HRS && totalWorkingDays<=NUM_OF_WORKING_DAYS)
-		{
-			totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;	//To generate a random valueof 0 to 2
+                int totalEmpWage=0;
+                int empWage=0;
+                int totalEmpHrs=0;
+                int totalWorkingDays=0;
+		/while loop for less than 20 working days and less than 100 working hours
+                while(totalEmpHrs<=maxWorkingHrs && totalWorkingDays<=numOfWorkingDays)
+                {
+                        totalWorkingDays++;
+                        int empCheck=(int)Math.floor(Math.random()*10)%3;		//To generate a random valueof 0 to 2
 
-			switch(empCheck)					//switch case for employee fulltime,parttime and absent
-			{
-				case FULL_TIME:
-				empHrs=8;
-				break;
-			
-				case PART_TIME:
-				empHrs=4;
-				break;
-		
-				default:
-				empHrs=0;
-			}
-			totalEmpHrs+=empHrs;					//Total working hours
-		}
-		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;                       //Salary of an employee
-		System.out.println("Total Employee Wage: "+totalEmpWage);
-		return totalEmpWage;
+                        switch(empCheck)						//switch case for employee fulltime,parttime and absent
+                        {
+                                case FULL_TIME:
+                                empHrs=8;
+                                break;
+
+                                case PART_TIME:
+                                empHrs=4;
+                                break;
+
+                                default:
+                                empHrs=0;
+                        }
+                        totalEmpHrs+=empHrs;
+                }
+                totalEmpWage=totalEmpHrs*empRatePerHr;							//Salary of an employee
+                System.out.println("Company: "+company+" Total Employee Wage: "+totalEmpWage);
+
 	}
-	public static void main(String[] args)
+
+	public static void main(String[] args)                                //Main method
 	{
-		computeEmpwage();                                               //Calling computeEmpWage method
+		computeEmpWage("Dmart",12,34,56);			      //Mentioning company,employee rate per hour,no. of working days and max working hours 
+		computeEmpWage("Reliance",23,36,46);
+		computeEmpWage("Deloitte",24,64,16);
 	}
 }
