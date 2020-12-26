d75c065a19d70b7a0c3d5bb5d607e3a80b7bddd8
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-26 14:28:22 +0530

    Add:Save total wage for each company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,24 +1,35 @@
-public class EmployeeWageComputation                               //Main class
+public class EmployeeWageComputation						//Main class
 {
 	//Constants
-	public static final int FULL_TIME=1;
-	public static final int PART_TIME=2;
+        public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
+	//Instance variables
+        public String company;
+        public int empRatePerHr;
+        public int numOfWorkingDays;
+        public int maxWorkingHrs;
 
-	public static void computeEmpWage(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)    //Method 
-	{
-		//Variables
-		int empHrs=0;
+        public EmployeeWageComputation(String company,int empRatePerHr,int numOfWorkingDays,int maxWorkingHrs)	//Parameterized constructor
+        {
+                this.company=company;
+                this.empRatePerHr=empRatePerHr;
+                this.numOfWorkingDays=numOfWorkingDays;
+                this.maxWorkingHrs=maxWorkingHrs;
+        }
+        public void computeEmpWage()						//Compute method
+        {
+                int empHrs=0;
                 int totalEmpWage=0;
                 int empWage=0;
                 int totalEmpHrs=0;
                 int totalWorkingDays=0;
-		/while loop for less than 20 working days and less than 100 working hours
+
                 while(totalEmpHrs<=maxWorkingHrs && totalWorkingDays<=numOfWorkingDays)
                 {
                         totalWorkingDays++;
-                        int empCheck=(int)Math.floor(Math.random()*10)%3;		//To generate a random valueof 0 to 2
+                        int empCheck=(int)Math.floor(Math.random()*10)%3;
 
-                        switch(empCheck)						//switch case for employee fulltime,parttime and absent
+                        switch(empCheck)					//switch case for employee fulltime,parttime and absent
                         {
                                 case FULL_TIME:
                                 empHrs=8;
@@ -33,15 +44,18 @@ public class EmployeeWageComputation                               //Main class
                         }
                         totalEmpHrs+=empHrs;
                 }
-                totalEmpWage=totalEmpHrs*empRatePerHr;							//Salary of an employee
-                System.out.println("Company: "+company+" Total Employee Wage: "+totalEmpWage);
+                totalEmpWage=totalEmpHrs*empRatePerHr;
+                System.out.println("Company: "+company+" Total Employee Wage: "+totalEmpWage);	//Total employee wage
 
-	}
+        }
 
-	public static void main(String[] args)                                //Main method
-	{
-		computeEmpWage("Dmart",12,34,56);			      //Mentioning company,employee rate per hour,no. of working days and max working hours 
-		computeEmpWage("Reliance",23,36,46);
-		computeEmpWage("Deloitte",24,64,16);
-	}
+        public static void main(String[] args)						//Main method
+        {
+                EmployeeWageComputation dmart=new EmployeeWageComputation("Dmart",12,34,56);
+                EmployeeWageComputation realiance=new EmployeeWageComputation("realiance",22,44,16);
+                EmployeeWageComputation deloitte=new EmployeeWageComputation("Deloitte",62,44,26);
+                dmart.computeEmpWage();
+                realiance.computeEmpWage();
+                deloitte.computeEmpWage();
+        }
 }
